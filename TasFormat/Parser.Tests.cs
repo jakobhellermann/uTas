fe03a73146c00c864abc9fd2884959f3e650ed3d
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-07-13 19:39:14 +0200

    add tas format parser

---
+++ TasFormat/Parser.Tests.cs
@@ -0,0 +1,158 @@
+using NUnit.Framework;
+
+namespace TasFormat;
+
+public class ParserTests {
+    [Test]
+    public void Test() {
+        var exampleTas = @"RecordCount: 500
+console load 1
+   1 # comment
+
+Set, Player.Speed.X, 100
+
+#Start
+  88
+#lvl_1
+   5,R,X
+   6,R,J
+";
+
+        Parser.Parse(exampleTas);
+    }
+
+    [Test]
+    public void ParsesWithoutException() {
+        var inputs = new[] {
+            @"#21:28.209
+#Start from Begin
+
+Read,StartFullGameFile
+
+Read,0 - Prologue,Start
+
+Read,LoadANoCollects,Prologue
+
+Read,1A,Start
+
+Read,LoadANoCollects
+
+Read,2A,Start
+
+Read,LoadANoCollects
+
+Read,3A,Start
+
+Read,LoadA
+
+Read,4A,Start
+
+Read,LoadANoCollects
+
+Read,5HC,Start,lvl_b-17 (1)
+
+#lvl_b-17
+   3,R,D
+   1,R,J
+   1,R
+  11,R,D,X
+   2,R,J,G
+  10,R
+   3,R,J
+   5,R
+  40
+
+#lvl_b-22
+  14,R,D,X
+   1,L,J
+   6,L,U,X
+   3,R,J
+   7,R
+  15,R,D,X
+  15,D,C
+   2
+   6,R,X
+  44,R,C
+  36,G
+   1
+   2,J
+   5,R,Z
+   4,R,J,G
+   6,R
+   9,R,J
+   4
+  15,R,D,X
+  15,D,C
+  10,L,X
+   6,R,J
+   1,R
+  15,R,X
+  10,R,C
+   9,R,J
+   7,R
+  15,U,Z
+   1,L,J,G
+   1,R,J
+   1,L,J
+   1,R
+   6,L
+   4,Z
+   1,L,J
+   6,L
+   2,L,D,J
+   4,L
+  14,L,X
+   1,L,J
+  15,D,C
+  15,D,X
+  14,L,Z
+   1,R,J
+  13,R,Z
+   1,L,J
+   1,R
+  14,L,D,X
+   1,L,J
+   3,L,K,G
+  15,L,D,X
+   1,L
+   8,L,J
+ 257
+   1,J
+
+Read,LoadBFromA
+
+Read,5B,Start
+
+Read,LoadAFromB
+
+Read,6HC,Start,Return
+
+Read,LoadBFromA
+
+Read,6B,Start
+
+Read,LoadAFromB,0,Summit
+#Summit
+  45
+Add 29
+   1,J
+  32
+Add 1
+
+Read,7A,Start
+
+Read,LoadCoreFromSummit,0,Core
+ 300
+   1,J
+Add 58
+ 427
+
+Read,LoadJournal,0,Any%
+"
+        };
+
+        foreach (var input in inputs) {
+            Parser.Parse(input);
+        }
+    }
+}
---
+++ TasFormat/Parser.cs
@@ -0,0 +1,116 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TasFormat;
+
+public readonly record struct Input(string Key) {
+    public override string ToString() {
+        return Key;
+    }
+}
+
+// ReSharper disable once InconsistentNaming
+public interface TasLine {
+    public record FrameInput(int FrameCount, List<Input> Inputs) : TasLine {
+        public override string ToString() {
+            return $"FrameInput {{ FrameCount = {FrameCount}, Inputs = {string.Join(',', Inputs)} }}";
+        }
+    }
+
+    public record Call(string Method, string[] Arguments) : TasLine {
+        public override string ToString() {
+            return $"Call {{ Method = {Method}, Arguments = {string.Join(',', Arguments)} }}";
+        }
+    }
+
+
+    public record Property(string Key, string Value) : TasLine;
+
+    public record Comment(string Text) : TasLine;
+}
+
+public record TasLineInfo(TasLine Line, int LineNumber);
+
+public record TasFile(List<TasLineInfo> Lines);
+
+public class Parser {
+    public static TasFile Parse(string file) {
+        var lines = file.Split("\n");
+
+        List<TasLineInfo> tasLines = new List<TasLineInfo>();
+
+        var lineNumber = -1;
+        foreach (var line in lines) {
+            lineNumber++;
+
+            var span = line.AsSpan();
+            var commentIndex = span.IndexOf('#');
+
+            if (commentIndex != -1) {
+                var beforeComment = span[..commentIndex];
+                var afterComment = span[(commentIndex + 1) ..];
+
+                if (beforeComment.Trim().IsWhiteSpace()) {
+                    tasLines.Add(new TasLineInfo(new TasLine.Comment(afterComment.ToString()), lineNumber));
+                    continue;
+                }
+
+                span = beforeComment;
+            }
+
+            if (span.IsWhiteSpace()) continue;
+
+            var commaIndex = span.IndexOf(',');
+            var beforeComma = commaIndex == -1 ? span : span[..commaIndex];
+            var afterComma = commaIndex == -1 ? ReadOnlySpan<char>.Empty : span[(commaIndex + 1)..];
+
+            if (int.TryParse(beforeComma, out var frameCount)) {
+                var inputs = new List<Input>();
+                var inputStrings = afterComma.ToString().Split(",");
+                foreach (var inputString in inputStrings) {
+                    var inputStringTrimmed = inputString.AsSpan().Trim();
+                    if (inputStringTrimmed.IsWhiteSpace()) continue;
+
+                    if (Regex.IsMatch(inputStringTrimmed, @"^[a-zA-Z]$")) {
+                        inputs.Add(new Input(inputStringTrimmed.ToString()));
+                    } else {
+                        throw new Exception($"unexpected input `{inputStringTrimmed}` in line `{afterComma}`");
+                    }
+                }
+
+                tasLines.Add(new TasLineInfo(new TasLine.FrameInput(frameCount, inputs), lineNumber));
+                continue;
+            }
+
+
+            var colonIndex = span.IndexOf(':');
+            if (colonIndex != -1) {
+                var key = span[..colonIndex].Trim();
+                var value = span[(colonIndex + 1)..].Trim();
+                tasLines.Add(new TasLineInfo(new TasLine.Property(key.ToString(), value.ToString()), lineNumber));
+                continue;
+            }
+
+
+            var method = beforeComma.ToString();
+            var arguments = afterComma.ToString().Split(',').Select(argument => argument.Trim()).ToArray();
+            tasLines.Add(new TasLineInfo(new TasLine.Call(method, arguments), lineNumber));
+        }
+
+        Console.WriteLine(string.Join('\n', tasLines.Select(line => line.ToString())));
+        return new TasFile(tasLines);
+    }
+
+
+/*
+ console a b c
+ Set,A,B,C
+ Invoke,A,B,C
+ Assert Equal True value
+ 
+ Block,Parameter
+ <inputs>
+ EndBlock
+ 
+ Value: x
+ */
+}
