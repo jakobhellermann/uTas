e34b6a6de8ce74139f431403e0decc87a08c8762
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-07-28 12:31:44 +0200

    add Ctrl[+Shift]+K to toggle comment

--- TasEditor/Services/TasEditingService.cs
+++ TasEditor/Services/TasEditingService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Immutable;
+using AvaloniaEdit.Document;
 using AvaloniaEdit.Editing;
 
 namespace TasEditor.Services;
@@ -6,14 +8,86 @@ namespace TasEditor.Services;
 public class TasEditingService : ITasEditingService {
     public TextArea TextArea = null!; // Inititalized by Editor.axaml.cs
 
+    private const char CommentToken = '#';
+
+    private int SelectionStartLine =>
+        Math.Min(TextArea.Selection.StartPosition.Line, TextArea.Selection.EndPosition.Line);
+
+    private int SelectionEndLine =>
+        Math.Max(TextArea.Selection.StartPosition.Line, TextArea.Selection.EndPosition.Line);
+
+
     public void ToggleCommentLineByLine() {
+        if (TextArea.Selection.Length == 0) {
+            var line = TextArea.Document.GetLineByNumber(TextArea.Caret.Line);
+            var text = GetText(line);
+            if (text.AsSpan().TrimStart().IsWhiteSpace()) return;
+
+            LineToggleComment(line, text);
+            TextArea.Caret.Line += 1;
+        } else {
+            var startLine = TextArea.Document.GetLineByNumber(SelectionStartLine);
+            var endLine = SelectionEndLine;
+
+            TextArea.Document.BeginUpdate();
+            for (var line = startLine; line != null && line.LineNumber <= endLine; line = line.NextLine) {
+                var text = GetText(line);
+                if (text.AsSpan().TrimStart().IsWhiteSpace()) continue;
+
+                LineToggleComment(line, text);
+            }
+
+            TextArea.Document.EndUpdate();
+            ExtendSelectionLineBoundaries();
+        }
     }
 
     public void ToggleComment() {
+        if (TextArea.Selection.Length == 0) {
+            ToggleCommentLineByLine();
+            return;
+        }
+
+        var startLine = TextArea.Document.GetLineByNumber(SelectionStartLine);
+        var endLine = SelectionEndLine;
+
+        var anyHasComment = false;
+        for (var line = startLine; line != null && line.LineNumber <= endLine; line = line.NextLine) {
+            var text = GetText(line);
+            var lineTrimmed = text.AsSpan().TrimStart();
+            if (lineTrimmed.IsWhiteSpace()) continue;
+            anyHasComment |= lineTrimmed.StartsWith(new ReadOnlySpan<char>(CommentToken));
+        }
+
+        TextArea.Document.BeginUpdate();
+        for (var line = startLine; line != null && line.LineNumber <= endLine; line = line.NextLine) {
+            var text = GetText(line);
+            var lineTrimmed = text.AsSpan().TrimStart();
+            if (lineTrimmed.IsWhiteSpace()) continue;
+            LineSetComment(line, text, !anyHasComment);
+        }
+        TextArea.Document.EndUpdate();
+        
+        ExtendSelectionLineBoundaries();
     }
 
     public void CombineConsecutiveInputs() {
     }
+
+    private void LineToggleComment(ISegment line, ReadOnlySpan<char> lineText) {
+        var hasComment = lineText.StartsWith(new ReadOnlySpan<char>(CommentToken));
+        LineSetComment(line, lineText, !hasComment);
+    }
+
+    private void LineSetComment(ISegment line, ReadOnlySpan<char> lineText, bool shouldHaveComment) {
+        if (shouldHaveComment) {
+            TextArea.Document.Insert(line.Offset, "#");
+        } else {
+            var commentIndex = lineText.IndexOf(CommentToken);
+            TextArea.Document.Replace(line.Offset, commentIndex + 1, "");
+        }
+    }
+
 
     public void ExtendSelectionLineBoundaries() {
         var selection = TextArea.Selection;
@@ -31,4 +105,7 @@ public class TasEditingService : ITasEditingService {
             TextArea.Selection = Selection.Create(TextArea, startOffset, endOffset);
         }
     }
+
+
+    private string GetText(ISegment line) => TextArea.Document.GetText(line.Offset, line.Length);
 }
